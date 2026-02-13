75b75872e359f40f8808c96df6f284624c71e55a
Paul Madden <136389411+maddenp-cu@users.noreply.github.com>
2026-02-12 19:59:43 -0700

    Package updates (#103)

--- .github/scripts/conda.sh
+++ .github/scripts/conda.sh
@@ -1,6 +1,6 @@
 . $(dirname ${BASH_SOURCE[0]})/common.sh
 set -eux
-url=https://github.com/conda-forge/miniforge/releases/download/25.11.0-1/Miniforge3-25.11.0-1-Linux-x86_64.sh
+url=https://github.com/conda-forge/miniforge/releases/download/26.1.0-0/Miniforge3-26.1.0-0-Linux-x86_64.sh
 installer=/tmp/$(basename $url)
 wget --no-verbose -O $installer $url
 bash $installer -bfp $CI_CONDA_DIR
